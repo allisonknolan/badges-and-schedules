8b0b2d198843a0711de14b1700731ba9c8bdc6e8
Allison Nolan <allisonknolan@gmail.com>
2020-11-22 16:13:26 -0600

    commit tree b615a49ed6f6330872b5207967ee6f30c0825c35

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,19 @@
-# Write your code here.
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+    
+def batch_badge_creator(attendees)
+  ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+  attendees.each do |name|
+    puts "Hello, my name is #{name}."
+  end
+end
+
+def assign_rooms(room_assignments)
+rooms = []
+room_assignments.each do |name, index|
+  puts "Hello, #{name}! You'll be assigned to room #{index + 1}!"
+  end
+rooms 
+end
+
